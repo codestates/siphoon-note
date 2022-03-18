d7168d9902e3d63407757d283541dd08af6e864c
Leekyeongmi <starry3ones@gmail.com>
2022-03-18 17:19:09 +0900

    Add: 개발 초기환경 세팅 완료
    CRA, eslint/prettier, 모듈 설치
    
    Resolves: #4

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <h1>hello!</h1>
+    //* 라우팅
+    // 내비게이션바
+    // 엔드포인트가 루트인 경우 landing page 컴포넌트로 이동
+    // 엔드포인트가 로그인인 경우, 로그인 컴포넌트
+    // 엔드포인트가 회원가입인 경우, 회원가입 컴포넌트
+    // 엔드포인트가 메인페이지인 경우, AuthRoute 컴포넌트로 이동 후, 유저용/체험용 컴포넌트로 이동
+  );
+}
+
+export default App;
