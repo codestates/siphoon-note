595ba8b7f448d9b4dee786df5c8d8fb8c5f40928
River Ann <maragriver@gmail.com>
2022-03-21 16:26:12 +0900

    Add: connectin test

--- server/index.js
+++ server/index.js
@@ -11,6 +11,22 @@ app.get('/', (req, res) => {
   res.status(200).send('Hello From Server!😀');
 });
 
+// 랜딩페이지 접속 에러 시 서버에서 처리
+app.use((req, res, next) => {
+  const err = new Error(`${req.method} ${req.url} Router Not Found`);
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: err,
+  });
+});
+// 랜딩페이지 접속 에러 시 서버에서 처리
+
 const server = http.createServer(app);
 
 server.listen(app.get('port'), () => {
