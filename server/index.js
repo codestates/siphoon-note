75860cccf91e01678aff760b1557df738a0a253f
River Ann <maragriver@gmail.com>
2022-03-21 21:55:49 +0900

    Fix: 로컬 포트와 parameter store의 포트 번호 추가

--- server/index.js
+++ server/index.js
@@ -4,7 +4,8 @@ const http = require('http');
 const cors = require('cors');
 const app = express();
 
-app.set('port', process.env.PORT || 5500);
+const port = process.env.LOCAL_PORT;
+
 app.use(cors());
 
 app.get('/', (req, res) => {
@@ -29,8 +30,8 @@ app.use((err, req, res, next) => {
 
 const server = http.createServer(app);
 
-server.listen(app.get('port'), () => {
-  console.log(`서버가 ${app.get('port')}에서 실행 중!`);
+server.listen(port, () => {
+  console.log(`서버가 ${port}에서 실행 중!`);
 });
 
 module.exports = server;
