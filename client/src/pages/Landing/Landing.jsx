2cf973e3f242540de4a8fa85060ed1a953e397ec
River Ann <maragriver@gmail.com>
2022-03-23 17:29:26 +0900

    Fix: url 수정

--- client/src/pages/Landing/Landing.jsx
+++ client/src/pages/Landing/Landing.jsx
@@ -1,12 +1,11 @@
 import { Container, Image, Main, Span, Button } from './Landing.style';
 import { NavLink } from 'react-router-dom';
-import config from '../../../config/config';
 import axios from 'axios';
 
 export default function Landing({ isLogin }) {
   //---------- 서버와 연결 확인용 코드 ----------
   axios
-    .get(config.GET_LANDING)
+    .get('http://ec2-3-38-168-114.ap-northeast-2.compute.amazonaws.com')
     .then(res => {
       console.log('Connection success', res);
     })
