9295448be65b196b9008c837640334e30d0b3bc5
lindist12 <lindist12@gmail.com>
2022-03-27 15:09:51 +0900

    Fix: 회원가입 기능 수정

--- client/src/components/Select/Selectbox.jsx
+++ client/src/components/Select/Selectbox.jsx
@@ -1,65 +1,3 @@
-// // // import React from 'react';
-// // // import Sel from './Select.style';
-
-// // import { useState } from 'react';
-// // import styled from 'styled-components';
-
-// // const SelectBoxWrapper = styled.div`
-// //   display: flex;
-// // `;
-// // const IconSVG = styled.svg`
-// //   margin-left: -28px;
-// //   align-self: center;
-// //   width: 24px;
-// //   height: 24px;
-// // `;
-
-// // // focus 시에 border-color를 red로 변경
-// // export const Select = styled.select`
-// //   margin: 0;
-// //   min-width: 0;
-// //   display: block;
-// //   width: 100%;
-// //   padding: 8px 8px;
-// //   font-size: inherit;
-// //   line-height: inherit;
-// //   border: 1px solid;
-// //   border-radius: 4px;
-// //   color: inherit;
-// //   -webkit-appearance: none;
-// //   -moz-appearance: none;
-// //   appearance: none;
-// //   background-color: transparent;
-// //   &:focus {
-// //     border-color: red;
-// //   }
-// // `;
-// export default function Selectbox({ option, title, value, name, onBlur }) {
-//   // const [userInfo, setUserInfo] = useState('');
-//   // console.log(userInfo);
-//   // handleInputValue = key => e => {
-//   //   setUserInfo({ ...userInfo, [key]: e.target.value });
-//   // };
-
-//   return (
-//     // <select onChange={() => setData(local.name)}>
-//     //   {local.map(option => (
-//     //     <option key={option.value} value={option.value} name={option.name}>
-//     //       {option.name}
-//     //     </option>
-//     //   ))}
-//     // </select>
-//     <select onBlur={event => onBlur(event.target.value)}>
-//       {option.map(({ title, value, name }) => {
-//         <option id={title} value={value} name={name}>
-//           {name}
-//         </option>;
-//       })}
-//     </select>
-//   );
-// }
-
-// // //https://blog.toycrane.xyz/react%EC%97%90%EC%84%9C-select-box-%EC%BB%B4%ED%8F%AC%EB%84%8C%ED%8A%B8-%EB%A7%8C%EB%93%A4%EA%B8%B0-a20e2bf082b2
 import React from 'react';
 import { Select, Input } from './Select.style';
 
@@ -81,8 +19,6 @@ export function Selectbox({ options, select, setSelect }) {
 
 export function Selectbox2({ gender, setGender, options }) {
   const handleSelect2 = e => {
-    if (gender === '') {
-    }
     setGender(e.target.value);
   };
   console.log(options[3]);
