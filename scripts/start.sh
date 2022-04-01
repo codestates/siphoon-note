ec908cbd940acae831b1ea607a6e924d5a91b122
River Ann <maragriver@gmail.com>
2022-04-01 13:28:08 +0900

    Remove: port env variable

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 cd /home/ubuntu/diary/server
-
-export EC2_PORT=$(aws ssm get-parameters --region ap-northeast-2 --names EC2_PORT --query Parameters[0].Value | sed 's/"//g')
-
 authbind --deep pm2 start index.js
