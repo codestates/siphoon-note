215567eaf0c6572f8be70135f6b969cbb10f23c9
River Ann <maragriver@gmail.com>
2022-04-05 12:09:32 +0900

    Add: 토큰이 필요한 로그인, 가입 로직 주석 추가

--- server/controllers/auth/index.js
+++ server/controllers/auth/index.js
@@ -1,11 +1,11 @@
-const { hasAuthorization } = require('./hasAuthorization');
-const { isValidateAuth } = require('./isValidateAuth');
 const { getRefreshToken } = require('./getRefreshToken');
+const { signup } = require('./signup');
+const { signin } = require('./signin');
 
 // 인증, 인가에 필요한 비지니스 로직
 // 토큰 관련 모듈을 utils로 분리하는 것으로 고려한다면, controller에서는 어떤 내용이 필요할지
 module.exports = {
-  hasAuthorization,
-  isValidateAuth,
   getRefreshToken,
+  signup,
+  signin,
 };
---
+++ server/controllers/auth/signin.js
@@ -0,0 +1,4 @@
+// 요청 객체에서 email, pw 받아오기
+// email, encrypted_pw로 계정 찾기
+//  있으면 계정의 refreshToken으로 accessToken 발급
+//  없으면 로그인 거절
