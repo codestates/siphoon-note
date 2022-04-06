76b1d5020d4f2a392cc1b501c1b33a5d8fc0b346
River Ann <maragriver@gmail.com>
2022-04-06 15:03:24 +0900

    Add: auth 컨트롤러가 로그아웃도 처리

--- server/controllers/auth/index.js
+++ server/controllers/auth/index.js
@@ -1,8 +1,10 @@
 const { getRefreshToken } = require('./getRefreshToken');
 const { login } = require('./login');
+const { logout } = require('./logout');
 
 // 인증, 인가에 필요한 비지니스 로직
 module.exports = {
   getRefreshToken,
   login,
+  logout,
 };
