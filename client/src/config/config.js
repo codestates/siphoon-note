cbe47b4d0e3aef07c319df9d0551df94e2b5cda7
River Ann <maragriver@gmail.com>
2022-04-09 19:04:03 +0900

    Add: version to api uri

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -27,110 +27,110 @@ const SERVER_HOST =
   'https://ec2-13-231-136-118.ap-northeast-1.compute.amazonaws.com';
 
 const apiUris = {
-  //------------- 1. main -------------
-  // GET /
-  LOAD_LANDING: `${SERVER_HOST}`,
-
-  //----------- 2. essay -------------
-  // POST /essays
-  WRITE_ESSAY: `${SERVER_HOST}/essays`,
-
-  // GET /essays
-  READ_ESSAY_LIST: `${SERVER_HOST}/essays`,
-
-  // GET /essays/:filter    ! query params로 필터링할 키워드 전달 필요
-  READ_ESSAY_LIST_BY_WORD: `${SERVER_HOST}/essays`,
-
-  // PATCH /essays/:essayId   ! query params로 essayId 전달 필요
-  DELETE_ESSAY_BY_ID: `${SERVER_HOST}/essays`,
-
-  // PUT /essays/:essayId   ! query params로 essayId 전달 필요
-  UPDATE_ESSAY_BY_ID: `${SERVER_HOST}/essays`,
-
-  //---------- 3. user --------------
-  // POST /signup
-  SIGN_UP: `${SERVER_HOST}/signup`,
-
-  // POST /signin
-  SIGN_IN: `${SERVER_HOST}/signin`,
-
-  // DELETE /signout
-  SIGN_OUT: `${SERVER_HOST}/signout`,
-
-  // GET /userinfo
-  READ_USER_INFO: `${SERVER_HOST}/userinfo`,
-
-  // PATCH /userinfo
-  UPDATE_USER_INFO: `${SERVER_HOST}/userinfo`,
-
-  // DELTE /userinfo
-  DELETE_ACCOUNT: `${SERVER_HOST}/userinfo`,
-
-  //------------ 4. trash --------------
-  // GET /trashes
-  READ_TRASH_LIST: `${SERVER_HOST}/trashes`,
-
-  // PATCH /trashes/:essayId  ! query params로 essayId 전달 필요
-  RESTORE_ESSAY_BY_ID: `${SERVER_HOST}/trashes`,
-
-  // DELETE /trashes/:essayId ! query params로 essayId 전달 필요
-  DELETE_TRASH_BY_ID: `${SERVER_HOST}/trashes`,
-
-  //------------ 5. tag --------------
-  // GET /tags
-  READ_TAG_LIST: `${SERVER_HOST}/tags`,
   // //------------- 1. main -------------
   // // GET /
-  // LOAD_LANDING: `${SERVER_HOST}/api/v1`,
+  // LOAD_LANDING: `${SERVER_HOST}`,
 
   // //----------- 2. essay -------------
   // // POST /essays
-  // WRITE_ESSAY: `${SERVER_HOST}/api/v1/essays`,
+  // WRITE_ESSAY: `${SERVER_HOST}/essays`,
 
   // // GET /essays
-  // READ_ESSAY_LIST: `${SERVER_HOST}/api/v1/essays`,
+  // READ_ESSAY_LIST: `${SERVER_HOST}/essays`,
 
   // // GET /essays/:filter    ! query params로 필터링할 키워드 전달 필요
-  // READ_ESSAY_LIST_BY_WORD: `${SERVER_HOST}/api/v1/essays`,
+  // READ_ESSAY_LIST_BY_WORD: `${SERVER_HOST}/essays`,
 
   // // PATCH /essays/:essayId   ! query params로 essayId 전달 필요
-  // DELETE_ESSAY_BY_ID: `${SERVER_HOST}/api/v1/essays`,
+  // DELETE_ESSAY_BY_ID: `${SERVER_HOST}/essays`,
 
   // // PUT /essays/:essayId   ! query params로 essayId 전달 필요
-  // UPDATE_ESSAY_BY_ID: `${SERVER_HOST}/api/v1/essays`,
+  // UPDATE_ESSAY_BY_ID: `${SERVER_HOST}/essays`,
 
   // //---------- 3. user --------------
   // // POST /signup
-  // SIGN_UP: `${SERVER_HOST}/api/v1/signup`,
+  // SIGN_UP: `${SERVER_HOST}/signup`,
 
   // // POST /signin
-  // SIGN_IN: `${SERVER_HOST}/api/v1/signin`,
+  // SIGN_IN: `${SERVER_HOST}/signin`,
 
   // // DELETE /signout
-  // SIGN_OUT: `${SERVER_HOST}/api/v1/signout`,
+  // SIGN_OUT: `${SERVER_HOST}/signout`,
 
   // // GET /userinfo
-  // READ_USER_INFO: `${SERVER_HOST}/api/v1/userinfo`,
+  // READ_USER_INFO: `${SERVER_HOST}/userinfo`,
 
   // // PATCH /userinfo
-  // UPDATE_USER_INFO: `${SERVER_HOST}/api/v1/userinfo`,
+  // UPDATE_USER_INFO: `${SERVER_HOST}/userinfo`,
 
   // // DELTE /userinfo
-  // DELETE_ACCOUNT: `${SERVER_HOST}/api/v1/userinfo`,
+  // DELETE_ACCOUNT: `${SERVER_HOST}/userinfo`,
 
   // //------------ 4. trash --------------
   // // GET /trashes
-  // READ_TRASH_LIST: `${SERVER_HOST}/api/v1/trashes`,
+  // READ_TRASH_LIST: `${SERVER_HOST}/trashes`,
 
   // // PATCH /trashes/:essayId  ! query params로 essayId 전달 필요
-  // RESTORE_ESSAY_BY_ID: `${SERVER_HOST}/api/v1/trashes`,
+  // RESTORE_ESSAY_BY_ID: `${SERVER_HOST}/trashes`,
 
   // // DELETE /trashes/:essayId ! query params로 essayId 전달 필요
-  // DELETE_TRASH_BY_ID: `${SERVER_HOST}/api/v1/trashes`,
+  // DELETE_TRASH_BY_ID: `${SERVER_HOST}/trashes`,
 
   // //------------ 5. tag --------------
   // // GET /tags
-  // READ_TAG_LIST: `${SERVER_HOST}/api/v1/tags`,
+  // READ_TAG_LIST: `${SERVER_HOST}/tags`,
+  //------------- 1. main -------------
+  // GET /
+  LOAD_LANDING: `${SERVER_HOST}/api/v1`,
+
+  //----------- 2. essay -------------
+  // POST /essays
+  WRITE_ESSAY: `${SERVER_HOST}/api/v1/essays`,
+
+  // GET /essays
+  READ_ESSAY_LIST: `${SERVER_HOST}/api/v1/essays`,
+
+  // GET /essays/:filter    ! query params로 필터링할 키워드 전달 필요
+  READ_ESSAY_LIST_BY_WORD: `${SERVER_HOST}/api/v1/essays`,
+
+  // PATCH /essays/:essayId   ! query params로 essayId 전달 필요
+  DELETE_ESSAY_BY_ID: `${SERVER_HOST}/api/v1/essays`,
+
+  // PUT /essays/:essayId   ! query params로 essayId 전달 필요
+  UPDATE_ESSAY_BY_ID: `${SERVER_HOST}/api/v1/essays`,
+
+  //---------- 3. user --------------
+  // POST /signup
+  SIGN_UP: `${SERVER_HOST}/api/v1/signup`,
+
+  // POST /signin
+  SIGN_IN: `${SERVER_HOST}/api/v1/signin`,
+
+  // DELETE /signout
+  SIGN_OUT: `${SERVER_HOST}/api/v1/signout`,
+
+  // GET /userinfo
+  READ_USER_INFO: `${SERVER_HOST}/api/v1/userinfo`,
+
+  // PATCH /userinfo
+  UPDATE_USER_INFO: `${SERVER_HOST}/api/v1/userinfo`,
+
+  // DELTE /userinfo
+  DELETE_ACCOUNT: `${SERVER_HOST}/api/v1/userinfo`,
+
+  //------------ 4. trash --------------
+  // GET /trashes
+  READ_TRASH_LIST: `${SERVER_HOST}/api/v1/trashes`,
+
+  // PATCH /trashes/:essayId  ! query params로 essayId 전달 필요
+  RESTORE_ESSAY_BY_ID: `${SERVER_HOST}/api/v1/trashes`,
+
+  // DELETE /trashes/:essayId ! query params로 essayId 전달 필요
+  DELETE_TRASH_BY_ID: `${SERVER_HOST}/api/v1/trashes`,
+
+  //------------ 5. tag --------------
+  // GET /tags
+  READ_TAG_LIST: `${SERVER_HOST}/api/v1/tags`,
 };
 
 export default apiUris;
